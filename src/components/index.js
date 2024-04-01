3d566e35ab529886349d0197022364196b506dd6
harwarl <harwarl87@gmail.com>
2024-04-01 02:11:56 +0100

    redirects for netlify

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as SearchForm } from "./SearchForm";
 export { default as CocktailList } from "./CocktailList";
+export { default as CocktailCard } from "./CockTailCard";
